320ec6745fa611b3cdb45f4cde3d81c98d463e8b
momin-abdurraheman <111493298+momin-abdurraheman@users.noreply.github.com>
2023-07-04 17:32:52 +0530

    Update backTracking3.java

--- backTracking3.java
+++ backTracking3.java
@@ -1,5 +1,45 @@
-public class backTacking3{
-  public static void main(){
-    System.out.println("Hello ");
-  }
+public class backTracking3 {
+    public static void main(String[] args) {
+        boolean[][] board = {
+                { true, true, true },
+                { true, false, true },
+                { true, true, true }
+
+        };
+        int[][] path = new int[board.length][board[0].length];
+        pathall(" ", board, 0, 0,path,1);
+
+    }
+
+    static void pathallPrint(String p, boolean[][] maze, int r, int c,int[][] path,int step) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            for(int[] arr: path){
+              System.out.println(Arrays.toString(arr));
+            }
+            return;
+        }
+        if (!maze[r][c]) {
+            return;
+        }
+        // I am considering this path in my path
+        maze[r][c] = false;
+        path[r][c] = step;
+        if (r < maze.length - 1) {
+            pathallPrint(p + 'D', maze, r + 1, c,path,step+1);
+        }
+        if (c < maze[0].length - 1) {
+            pathallPrint(p + 'R', maze, r, c + 1,path,step+1);
+        }
+        if (r > 0) {
+            pathallPrint(p + 'U', maze,r - 1, c,path,step+1);
+        }
+        if (c > 0) {
+            pathallPrint(p + 'L', maze, r , c - 1,path,step+1);
+        }
+        // This is where function is over and will go back so while going back mark
+        // the block as true, This is known as BACKTACKING
+
+        maze[r][c] = true;
+        path[r][c] = 0;
+    }
 }

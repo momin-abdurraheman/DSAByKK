5c65c711759383ab30a78e6268ef14acf64060d1
momin-abdurraheman <111493298+momin-abdurraheman@users.noreply.github.com>
2023-07-04 17:35:47 +0530

    Update backTracking3.java

--- backTracking3.java
+++ backTracking3.java
@@ -13,9 +13,13 @@ public class backTracking3 {
 
     static void pathallPrint(String p, boolean[][] maze, int r, int c,int[][] path,int step) {
         if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c]=step;
             for(int[] arr: path){
               System.out.println(Arrays.toString(arr));
             }
+            System.out.println(p);
+            System.out.println();
+            
             return;
         }
         if (!maze[r][c]) {
